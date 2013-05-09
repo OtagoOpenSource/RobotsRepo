8dab6883907758a2adcc13cf7fcfe07a68503490
Brendan McCane <mccane@cs.otago.ac.nz>
2013-05-09 18:03:40 +1200

    Added a very simple editor using Python and Tkinter. Still lots to do, but I thought it worthwhile just to come up with a starting point and see what happens. Next step is for someone else to do something!

---
+++ Python_src/SimpleEditor.py
@@ -0,0 +1,141 @@
+# File: SimpleEditor.py
+# Created: 9th May, 2013
+# Creator: Brendan McCane
+# License: not sure yet, some open source license
+
+import Tkinter
+import ScrolledText
+import tkFileDialog
+import subprocess
+
+class SimpleEditor:
+    """
+    A simple editor for NQC code.
+    """
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.textWidget = ScrolledText.ScrolledText(parent, width=80, height=50)
+        self.textWidget.grid(row=0,column=0)
+        # this is for the output from the compiler
+        self.compilerWidget = ScrolledText.ScrolledText(parent, width=80, 
+                                                        height=20)
+        self.compilerWidget.grid(row=1,column=0)
+
+        # set up the menus
+        # todo - add shortcuts and possible shortcut icons
+        self.menuBar = Tkinter.Menu(parent, tearoff=0)
+        self.fileMenu = Tkinter.Menu(self.menuBar, tearoff=0)
+        self.fileMenu.add_command(label="New", command=self.new_command)
+        self.fileMenu.add_command(label="Open", command=self.open_command)
+        self.fileMenu.add_command(label="Save", command=self.save_command)
+        self.fileMenu.add_command(label="Save As", command=self.saveas_command)
+        self.fileMenu.add_command(label="Close", command=self.close_command)
+        self.fileMenu.add_command(label="Quit", command=self.quit_command)
+        self.menuBar.add_cascade(label="File", menu=self.fileMenu)
+        
+        self.commandMenu = Tkinter.Menu(self.menuBar, tearoff=0)
+        self.commandMenu.add_command(label="Check Syntax", 
+                                     command=self.check_syntax)
+        self.commandMenu.add_command(label="Compile and Upload", 
+                                     command = self.compile_upload)
+        self.menuBar.add_cascade(label="Compile", menu=self.commandMenu)
+
+        parent.config(menu=self.menuBar)
+
+        # initialise the file associated with this window as ''
+        self.filename = ''
+
+    
+    def new_command(self):
+        """
+        Create a blank new text window
+        """
+        root = Tkinter.Tk()
+        new_editor = SimpleEditor(root)
+        root.mainloop()
+        return new_editor
+
+    def open_command(self):
+        """
+        Open an existing file in the current window
+        """
+        if self.filename=='':
+            self.filename = tkFileDialog.askopenfilename()
+            if self.filename != '':
+                file = open(self.filename, "r")
+                contents = file.read()
+                self.textWidget.insert(Tkinter.END, contents)
+                file.close()
+        else:
+            editor = self.new_command()
+
+    def save_command(self):
+        """
+        Save the current contents to the current file.
+        """
+        if self.filename=='':
+            self.saveas_command()
+        else:
+            file = open(self.filename, "w")
+            contents = self.textWidget.get("1.0", Tkinter.END)[:-1]
+            file.write(contents)
+            file.close()
+
+    def saveas_command(self):
+        """
+        Save the current contents to an as yet unnamed file
+        """
+        self.filename = tkFileDialog.asksaveasfilename()
+        if self.filename != '':
+            file = open(self.filename, "w")
+            contents = self.textWidget.get("1.0", Tkinter.END)[:-1]
+            file.write(contents)
+            file.close()
+
+    def close_command(self):
+        """
+        Close this file. Should check to see if saving is needed.
+        Warning: this creates strange behaviour with multiple windows.
+        A more considered system is needed.
+        """
+        if self.textWidget.edit_modified():
+            self.save_command()
+        self.parent.quit()
+
+    def quit_command(self):
+        """
+        Quit the whole application. This is more difficult. Really
+        needs a singleton to keep track of all windows and send close
+        actions to those windows.
+        """
+        self.close_command()
+
+    def check_syntax(self):
+        """
+        Check the syntax of current contents by calling the NBC
+        compiler. Needs to create a sub window for the output of the
+        compiler and in the case of errors parse the output and move
+        the cursor to the appropriate line.
+        """
+        if self.textWidget.edit_modified():
+            self.save_command()
+        compile = subprocess.check_output(['../NBC_Mac/nbc', 
+                                           self.filename, 
+                                           '-O={}.rxe'.format(self.filename)],
+                                          stderr=subprocess.STDOUT)
+        self.compilerWidget.delete("1.0", Tkinter.END)
+        self.compilerWidget.insert(Tkinter.END, compile)
+
+    def compile_upload(self):
+        """
+        Compile and upload the program to an actual lego brick. Again
+        using the NBC compiler.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    root = Tkinter.Tk()
+    editor = SimpleEditor(root)
+    root.mainloop()
